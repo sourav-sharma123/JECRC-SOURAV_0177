c104d588ee0b8de3f670769473859bc488c0bf2b
Sourav <sourav.sharma@girnarsoft.co.in>
2026-03-18 13:42:46 +0530

    block-directive

---
+++ week-4-angular_basics/18-03-2026/assignments04/click-block-directive/src/app/click-block.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ClickBlockDirective } from './click-block.directive';
+
+describe('ClickBlockDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ClickBlockDirective();
+    expect(directive).toBeTruthy();
+  });
+});
