4f8b95b04fba80e88f2176bff8a961633ee8dc94
Sourav <sourav.sharma@girnarsoft.co.in>
2026-04-16 13:04:44 +0530

    .netcore example

---
+++ week-9-.netcore/case_studies/StudentManagementSystem/StudentAttendenceSystem/Controllers/AttendenceController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentAttendenceSystem.Model;
+
+namespace StudentAttendenceSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AttendenceController : ControllerBase
+    {
+        private static List<Attendence> attendenceRecords = new();
+
+        [HttpGet]
+        public IActionResult Get() => Ok(attendenceRecords);
+
+        [HttpPost]
+        public IActionResult Add(Attendence attendence)
+        {
+            attendenceRecords.Add(attendence);
+            return Ok(attendence);
+        }
+    }
+}
