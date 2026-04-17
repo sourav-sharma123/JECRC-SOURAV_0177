9e8f4d71fea336bfba4879d5cb690d79be7ef3b7
sourav-sharma123 <souravsharma.it26@jecrc.ac.in>
2026-04-17 18:46:31 +0530

    Delete week-4-angular_basics directory

--- week-4-angular_basics/18-03-2026/assignments05/status-color-directive/src/app/status-color.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { StatusColorDirective } from './status-color.directive';
-
-describe('StatusColorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new StatusColorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
